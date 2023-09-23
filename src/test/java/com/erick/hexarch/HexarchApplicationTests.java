2d15b2869cb72015170af72fd3d895467042767e
Erick Dourado <erickdourado_@hotmail.com>
2023-09-22 22:54:41 -0300

    Start project

---
+++ src/test/java/com/erick/hexarch/HexarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.hexarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
