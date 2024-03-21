bff9e1c34273dd352df33a7f9d51c73b378e7391
Erick Dourado <erickdourado_@hotmail.com>
2024-03-20 23:54:21 -0300

    Fix: bug resolution between spring version 3.0 and OpenFeign

--- src/main/java/com/erick/hexarch/HexarchApplication.java
+++ src/main/java/com/erick/hexarch/HexarchApplication.java
@@ -1,11 +1,14 @@
 package com.erick.hexarch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @EnableFeignClients
 @SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class HexarchApplication {
 
 	public static void main(String[] args) {
